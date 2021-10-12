ec09cc2f1baee1b1200b011492feb1e6cdd2efd3
Aaron Lin <aaronlin74@gmail.com>
2021-10-12 19:22:07 +1100

    Add more animal images

--- src/data.js
+++ src/data.js
@@ -2,6 +2,32 @@ import lion_img from './resources/images/lion.jpg'
 import lion_aud from './resources/audios/lion.mp3'
 import tiger_img from './resources/images/tiger.jpg'
 import tiger_aud from './resources/audios/tiger.mp3'
+import bear_img from './resources/images/bear.jpg'
+import bear_aud from './resources/audios/bear.mp3'
+import cow_img from './resources/images/cow.jpg'
+import cow_aud from './resources/audios/cow.mp3'
+import dog_img from './resources/images/dog.jpg'
+import dog_aud from './resources/audios/dog.mp3'
+import cat_img from './resources/images/cat.jpg'
+import cat_aud from './resources/audios/cat.mp3'
+import elephant_img from './resources/images/elephant.jpg'
+import elephant_aud from './resources/audios/elephant.mp3'
+import donkey_img from './resources/images/donkey.jpg'
+import donkey_aud from './resources/audios/donkey.mp3'
+import wolf_img from './resources/images/wolf.jpg'
+import wolf_aud from './resources/audios/wolf.mp3'
+import frog_img from './resources/images/frog.jpg'
+import frog_aud from './resources/audios/frog.mp3'
+import goat_img from './resources/images/goat.jpg'
+import goat_aud from './resources/audios/goat.mp3'
+import rooster_img from './resources/images/rooster.jpg'
+import rooster_aud from './resources/audios/rooster.mp3'
+import duck_img from './resources/images/duck.jpg'
+import duck_aud from './resources/audios/duck.mp3'
+import horse_img from './resources/images/horse.jpg'
+import horse_aud from './resources/audios/horse.mp3'
+import monkey_img from './resources/images/monkey.jpg'
+import monkey_aud from './resources/audios/monkey.mp3'
 
 export default [
   {
@@ -17,5 +43,96 @@ export default [
     image: tiger_img,
     audio: tiger_aud,
     link: 'https://en.wikipedia.org/wiki/Tiger',
+  },
+  {
+    id: 3,
+    name: 'bear',
+    image: bear_img,
+    audio: bear_aud,
+    link: 'https://en.wikipedia.org/wiki/Bear',
+  },
+  {
+    id: 4,
+    name: 'cow',
+    image: cow_img,
+    audio: cow_aud,
+    link: 'https://en.wikipedia.org/wiki/Cow',
+  },
+  {
+    id: 5,
+    name: 'dog',
+    image: dog_img,
+    audio: dog_aud,
+    link: 'https://en.wikipedia.org/wiki/Tiger',
+  },
+  {
+    id: 6,
+    name: 'cat',
+    image: cat_img,
+    audio: cat_aud,
+    link: 'https://en.wikipedia.org/wiki/Cat',
+  },
+  {
+    id: 7,
+    name: 'elephant',
+    image: elephant_img,
+    audio: elephant_aud,
+    link: 'https://en.wikipedia.org/wiki/Elephant',
+  },
+  {
+    id: 8,
+    name: 'donkey',
+    image: donkey_img,
+    audio: donkey_aud,
+    link: 'https://en.wikipedia.org/wiki/Donkey',
+  },
+  {
+    id: 9,
+    name: 'wolf',
+    image: wolf_img,
+    audio: wolf_aud,
+    link: 'https://en.wikipedia.org/wiki/Wolf',
+  },
+  {
+    id: 10,
+    name: 'frog',
+    image: frog_img,
+    audio: frog_aud,
+    link: 'https://en.wikipedia.org/wiki/Frog',
+  },
+  {
+    id: 11,
+    name: 'goat',
+    image: goat_img,
+    audio: goat_aud,
+    link: 'https://en.wikipedia.org/wiki/Goat',
+  },
+  {
+    id: 12,
+    name: 'rooster',
+    image: rooster_img,
+    audio: rooster_aud,
+    link: 'https://en.wikipedia.org/wiki/Rooster',
+  },
+  {
+    id: 13,
+    name: 'duck',
+    image: duck_img,
+    audio: duck_aud,
+    link: 'https://en.wikipedia.org/wiki/Duck',
+  },
+  {
+    id: 14,
+    name: 'horse',
+    image: horse_img,
+    audio: horse_aud,
+    link: 'https://en.wikipedia.org/wiki/Horse',
+  },
+  {
+    id: 15,
+    name: 'monkey',
+    image: monkey_img,
+    audio: monkey_aud,
+    link: 'https://en.wikipedia.org/wiki/Monkey',
   },
 ]
