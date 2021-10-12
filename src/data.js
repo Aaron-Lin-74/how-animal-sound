d09fa4fa192fa6e0cdd759356d32968b0a324499
Aaron Lin <aaronlin74@gmail.com>
2021-10-12 14:57:54 +1100

    Initial commit, set up the structure for the app

---
+++ src/data.js
@@ -0,0 +1,21 @@
+import lion_img from './resources/images/lion.jpg'
+import lion_aud from './resources/audios/lion.mp3'
+import tiger_img from './resources/images/tiger.jpg'
+import tiger_aud from './resources/audios/tiger.mp3'
+
+export default [
+  {
+    id: 1,
+    name: 'lion',
+    image: lion_img,
+    audio: lion_aud,
+    link: 'https://en.wikipedia.org/wiki/Lion',
+  },
+  {
+    id: 2,
+    name: 'tiger',
+    image: tiger_img,
+    audio: tiger_aud,
+    link: 'https://en.wikipedia.org/wiki/Tiger',
+  },
+]
