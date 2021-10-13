38bff2032acf763b33ed3f1d97be93f412347a4e
Aaron Lin <aaronlin74@gmail.com>
2021-10-13 12:37:08 +1100

    Add logo and impletement basic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,34 @@
 import React from 'react'
-
+import logo from '../logo.svg'
+import { FcMenu } from 'react-icons/fc'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
-  return <div>This is the Navbar component</div>
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' className='logo' />
+          <button className='nav-toggle'>
+            <FcMenu />
+          </button>
+        </div>
+
+        <div className='nav-links'>
+          <ul className='links'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/play'>Play</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
