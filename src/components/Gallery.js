a6649f8c39bd3f2b8e35fe8e46d4666223a11c89
Aaron Lin <aaronlin74@gmail.com>
2022-01-15 22:53:28 +1100

    Refactor the code using styled component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useGlobalContext } from '../context'
-import AnimalCard from './AnimalCard'
+import styled from 'styled-components'
+import load from '../resources/gifs/loading2.gif'
 import Card from './Card'
 import {
   FcAlphabeticalSortingAz,
@@ -21,37 +22,37 @@ const Gallery = () => {
   } = useGlobalContext()
 
   return (
-    <div className='gallery-container'>
-      <div className='btns-container'>
-        <button className='utl-btns' onClick={sortAnimals}>
+    <GalleryContainer>
+      <BtnsContainer>
+        <Button onClick={sortAnimals}>
           <FcAlphabeticalSortingAz />
           Sort A-Z{' '}
-        </button>
-        <button className='utl-btns' onClick={sortAnimalsDesc}>
+        </Button>
+        <Button onClick={sortAnimalsDesc}>
           <FcAlphabeticalSortingZa />
           Sort Z-A{' '}
-        </button>
-        <button className='utl-btns' onClick={shuffleAnimals}>
+        </Button>
+        <Button onClick={shuffleAnimals}>
           <FcProcess />
           Shuffle
-        </button>
-        <button className='utl-btns' onClick={toggleMini}>
+        </Button>
+        <Button onClick={toggleMini}>
           <FcGrid />
           {showMini ? 'Normal' : 'Mini'}
-        </button>
-      </div>
+        </Button>
+      </BtnsContainer>
       <div className={`${showMini ? 'card-grid-mini' : 'card-grid'}`}>
         {loading ? (
-          <img
-            className='loading'
-            src='https://www.google.com/images/spin-32.gif?a'
-            alt='loading'
-          />
+          <Loading>
+            <ImgContainer>
+              <Image src={load} alt='loading...' />
+            </ImgContainer>
+          </Loading>
         ) : (
           animals.map((animal) => {
             return (
               <Card
-                key={animal.id}
+                key={Math.random()}
                 name={animal.name}
                 imageURL={animal.imageURL}
                 audioURL={animal.audioURL}
@@ -61,8 +62,66 @@ const Gallery = () => {
           })
         )}
       </div>
-    </div>
+    </GalleryContainer>
   )
 }
 
 export default Gallery
+
+const GalleryContainer = styled.div`
+  position: relative;
+  margin-top: 100px;
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+  padding: 0 2rem;
+  min-height: 100vh;
+  background: var(--backgroundColor);
+`
+const BtnsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--backgroundColor);
+`
+
+const Button = styled.button`
+  padding: 12px;
+  font-size: 1.5rem;
+  background: var(--backgroundColor);
+  color: #fff;
+  width: 150px;
+  height: 100px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: none;
+  border: none;
+
+  &:hover {
+    background: rgb(19, 19, 19);
+  }
+`
+const Loading = styled.div`
+  position: fixed;
+  top: 70px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+  object-fit: cover;
+  object-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(195, 195, 197, 0.6);
+`
+const ImgContainer = styled.div`
+  width: 40vw;
+  max-width: 300px;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+`
